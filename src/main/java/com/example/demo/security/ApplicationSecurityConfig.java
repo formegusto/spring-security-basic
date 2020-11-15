0a231d4358de3c9b5870c30fd0d39d54c390f8bb
formegusto <formegusto@gmail.com>
2020-11-15 23:04:41 +0900

    ANT MATCHERS, PASSWORD ENCODE, PERMISSIONS

---
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.example.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // TODO Auto-generated method stub
+        http
+            .authorizeRequests()
+            .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Override
+    @Bean
+	protected UserDetailsService userDetailsService() {
+        // TODO Auto-generated method stub
+        UserDetails annaSmithUser = User.builder()
+            .username("annasmith")
+            .password(passwordEncoder.encode("password"))
+            .roles("STUDENT") // ROLE_STUDENT
+            .build();
+
+        UserDetails lindaUser = User.builder()
+            .username("linda")
+            .password(passwordEncoder.encode("password123"))
+            .roles("ADMIN") // ROLE_ADMIN
+            .build();
+
+        return new InMemoryUserDetailsManager(
+            annaSmithUser,
+            lindaUser
+        );
+	}
+
+}
